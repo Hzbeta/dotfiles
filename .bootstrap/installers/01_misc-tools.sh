96b05e0a6938b9c9f891bad402642389fa24abc9
Hzbeta <ihzbeta@outlook.com>
2023-07-22 15:20:28 +0800

    ✨ feat(bootstrap): add `dust` and `duf` and more
    - add `dust` and `duf` to `misc-tool,sh`
    - now `misc-tool.sh` will only use nix to install

--- .bootstrap/installers/01_misc-tools.sh
+++ .bootstrap/installers/01_misc-tools.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
 # tools list
-# we can use `exa,brew` to install `exa` with homebrew
+# we use `nix` to install tools
+# if the tool command is different from the package name
+# we use `tool,package` to specify the package name
 tools_info_list=(
-    'shellcheck,nix'
-    'tmux,nix'
-    'fzf,nix'
-    'lsd,nix'
-    'fd,nix'
-    'joshuto,nix'
-    'act,nix'
-)
+    'shellcheck'                  # ShellCheck, a static analysis tool for shell scripts.                         https://github.com/koalaman/shellcheck
+    'tmux'                        # tmux is a terminal multiplexer for Unix-like operating systems.               https://github.com/tmux/tmux
+    'fzf'                         # fzf is a general-purpose command-line fuzzy finder.                           https://github.com/junegunn/fzf
+    'lsd'                         # lsd is a modern replacement for ls.                                           https://github.com/lsd-rs/lsd
+    'fd'                          # fd is a simple, fast and user-friendly alternative to 'find'.                 https://github.com/sharkdp/fd
+    'joshuto'                     # joshuto is a minimalistic file manager in rust.                               https://github.com/kamiyaa/joshuto
+    'act'                         # act allows you to run your GitHub Actions locally.                            https://github.com/nektos/act
+    'dust,du-dust'                # dust is a more intuitive version of du in rust.                               https://github.com/bootandy/dust
+    'duf'                         # duf is a Disk Usage/Free Utility - a better 'df' alternative.                 https://github.com/muesli/duf
+)    
 
-# processing tools list
-for i in "${!tools_info_list[@]}"; do
-    # check if an installer is present
-    if [[ ${tools_info_list[i]} == *,* ]]; then
-        # append default installer
-        tools_info_list[i]="${tools_info_list[i]}:default"
-    else
-        # add default installer
-        tools_info_list[i]="${tools_info_list[i]},default"
-    fi
-done
 
 # this installer is for miscellaneous tools
 # don't need to check if it's installed or not
@@ -40,30 +33,21 @@ function check_is_misc-tools_installed() {
 # install
 function install_misc-tools() {
 
+    if ! command -v nix-env &>/dev/null; then
+        log warning "nix-env not found, please install nix first"
+        return 1
+    fi
+
     for tool_info in "${tools_info_list[@]}"; do
         tool="${tool_info%%,*}"
-        install_methods="${tool_info##*,}"
-        for method in $(echo "$install_methods" | tr ':' '\n'); do
-            if command -v "$tool" &>/dev/null; then
-                break
-            fi
-            if [[ "$method" == "nix" ]]; then # if the method is nix
-                if command -v nix-env &>/dev/null; then
-                    nix-env -iA nixpkgs."$tool" && break
-                fi
-            elif [[ "$method" != "default" ]]; then # if the method is not default
-                if command -v "$method" &>/dev/null; then
-                    "$method" install "$tool" && break
-                fi
-            fi
-            # if the method is default or the specified method command is not found
-            method="$(get_package_manager "install")"
-            if [[ "$method" == "Unknown" ]]; then
-                log error "Unknown Linux Distro, please install $tool manually."
-                return 1
-            fi
-            $method "$tool" && break
-        done
+        nix_package="${tool_info##*,}"
+        if [ "$nix_package" == "" ]; then
+            nix_package="$tool"
+        fi
+        if ! command -v "$tool" &>/dev/null; then
+            log info "installing $tool..."
+            nix-env -iA nixpkgs."$nix_package"
+        fi
         if ! command -v "$tool" &>/dev/null; then
             log error "failed to install $tool"
             return 1
